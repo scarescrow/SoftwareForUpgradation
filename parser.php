4ee0401c947330bebf28d960e348253bfe3780bd
sagnikroy <sagnik.sinharoy@gmail.com>
2014-07-15 10:01:27 +0530

    Major Problems resolved

--- parser.php
+++ parser.php
@@ -32,11 +32,11 @@
 	
 	//First, get all colleges in dictionary format
 	// Array("college_name" => array("branch_name"=>"no_allowed",...),...)
-	$arr_of_colleges = array();
+	$arr_of_colleges = Array();
 	
-	//Keep an array of all upgraded roll numbers
-	$arr_roll_nos = array();
-
+	//Array of upgraded students
+	$final_arr = Array();
+	
 	$query_get_colleges = "SELECT College FROM upgradation_college GROUP BY College";
 	$result = mysql_query($query_get_colleges, $con);
 	$num_rows = mysql_num_rows($result);
@@ -67,84 +67,106 @@
 	//Now, we have all colleges. So, first we iterate over all colleges.
 
 	$colleges = array_keys($arr_of_colleges);
-	for($i = 0;$i < count($colleges); $i++) {
+	for($i = 0;$i < count($colleges); $i++) {	//count($colleges)
 
 		$college = $colleges[$i];
 		
 		$diff = 1;
 		//Now, get all students from this college in descending order.
-			$diff = parse($college);
-			
-		
-	}
-	echo count($arr_roll_nos).'<br>';
-	print_r($arr_of_colleges);
-
-	//Cleaner Function
-	function clean($str) {
-		
-		$str = str_replace("'", "", $str);
-		$str = str_replace("/", "", $str);
-		$str = str_replace("\\", "", $str);
-		$str = str_replace(" ", "", $str);
-		
-		return $str;
-	}
-	
-	function parse($college) {
-	
-		global $arr_of_colleges, $arr_roll_nos, $con;
-		$old_array = $arr_of_colleges;
-		
-		$roll_nos = implode(',', $arr_roll_nos);
-		if(strlen($roll_nos) < 1)
-			$roll_nos = "1";
-			
-		$query_students = "SELECT * FROM upgradation_final WHERE Institute='$college' AND Roll_No NOT IN ('$roll_nos') ORDER BY Percentage DESC";
+		$query_students = "SELECT * FROM upgradation_final WHERE Institute='$college' ORDER BY Percentage DESC";
 		$result = mysql_query($query_students, $con) or die(mysql_error());
 		$num_rows = mysql_num_rows($result);
+		//Array to store student data of each college.
+		$arr_of_students = Array();
 		
 		for ($j = 0; $j < $num_rows; $j++) {
 			
-			$branch = mysql_result($result, $j, "Branch");
-			$roll_no = mysql_result($result, $j, "Roll_No");
+			//Array to store data of each student
+			$student = Array();
+			
+			$student['roll'] = mysql_result($result, $j, "Roll_No");
+			$student['branch'] = mysql_result($result, $j, "Branch");
+			$student['college'] = $college;
+			$student['name'] = mysql_result($result, $j, "Name");
+			$student['pref1'] = mysql_result($result, $j, "Pref1");
+			$student['pref2'] = mysql_result($result, $j, "Pref2");
+			$student['pref3'] = mysql_result($result, $j, "Pref3");
+			$student['pref4'] = mysql_result($result, $j, "Pref4");
+			$student['new_branch'] = "";
+			
+			$arr_of_students[] = $student;
+		
+		}
+		$arr_of_students = parse($arr_of_students, count($arr_of_students));	
+		
+		for($c = 0; $c < count($arr_of_students); $c++) {
+		
+			if(strlen($arr_of_students[$c]["new_branch"]) >= 2) {
+			
+				$roll = $arr_of_students[$c]['roll'];
+				$old_branch = $arr_of_students[$c]['branch'];
+				$new_branch = $arr_of_students[$c]['new_branch'];
+				$name = $arr_of_students[$c]['name'];
+				$query_final = "INSERT INTO upgradation_result (Roll_No, Name, Institute, Branch_Old, Branch_New)
+									VALUES ('$roll', '$name', '$college', '$old_branch', '$new_branch');";
+									
+				$result_final = mysql_query($query_final) or die(mysql_error());
+			
+			}
+			
+			
+	
+		}	
+		
+		
+	}
+	
+	
+	
+	
+	//Main Parser Function
+	function parse($arr_of_students, $main_count) {
+	
+		global $arr_of_colleges, $con;
+		
+		for ($k = 0; $k < $main_count; $k++) {
+			
+			$branch = $arr_of_students[$k]["branch"];
+			$roll_no = $arr_of_students[$k]["roll"];
+			$college = $arr_of_students[$k]["college"];
 			
 			//Now, check for each preference, and check if seats are available.
 			//If they are, then update branch, and push to database.
 			
-			$pref1 = clean(mysql_result($result, $j, "Pref1"));
-			$pref2 = clean(mysql_result($result, $j, "Pref2"));
-			$pref3 = clean(mysql_result($result, $j, "Pref3"));
-			$pref4 = clean(mysql_result($result, $j, "Pref4"));
+			$pref1 = clean($arr_of_students[$k]["pref1"]);
+			$pref2 = clean($arr_of_students[$k]["pref2"]);
+			$pref3 = clean($arr_of_students[$k]["pref3"]);
+			$pref4 = clean($arr_of_students[$k]["pref4"]);
 			
 			$found = false;
 			
-			
-			if ($arr_of_colleges[$college][$pref1][0] > 0 && $arr_of_colleges[$college][$pref1][0] < $arr_of_colleges[$college][$pref1][1]) {
+			if ($arr_of_colleges[$college][$pref1][0] > 0) {
 
 				$found = true;
 				$new_branch = $pref1;
 				
-				//Add roll no to array.
-				$arr_roll_nos[] = $roll_no; 
-			
 			} else if ($pref2 != '--' && $pref2 != $pref1 && $pref2 != $branch) {
 			
-				if ($arr_of_colleges[$college][$pref2][0] > 0 && $arr_of_colleges[$college][$pref2][0] < $arr_of_colleges[$college][$pref2][1]) {
+				if ($arr_of_colleges[$college][$pref2][0] > 0) {
 				
 					$found = true;
 					$new_branch = $pref2;
 				
 				} else if ($pref3 != '--' && $pref3 != $pref2 && $pref3 != $branch) {
 				
-					if ($arr_of_colleges[$college][$pref3][0] > 0 && $arr_of_colleges[$college][$pref3][0] < $arr_of_colleges[$college][$pref3][1]) {
+					if ($arr_of_colleges[$college][$pref3][0] > 0) {
 					
 						$found = true;
 						$new_branch = $pref3;
 					
 					} else if ($pref4 != '--' && $pref4 != $pref3 && $pref4 != $branch) {
 					
-						if($arr_of_colleges[$college][$pref4][0] > 0 && $arr_of_colleges[$college][$pref4][0] < $arr_of_colleges[$college][$pref4][1]) {
+						if($arr_of_colleges[$college][$pref4][0] > 0) {
 
 							$found = true;
 							$new_branch = $pref4;
@@ -159,31 +181,39 @@
 			
 			if($found == true) {
 			
-				if(!isset($arr_of_colleges[$college][$branch][0]))
-					echo $college;
-				if ($arr_of_colleges[$college][$branch][0] < $arr_of_colleges[$college][$branch][1])
+				if($arr_of_students[$k]["new_branch"] != $new_branch) {
+				
+					if(strlen($arr_of_students[$k]["new_branch"] >= 2))						
+						$branch = $arr_of_students[$k]["new_branch"];
+					
 					$arr_of_colleges[$college][$branch][0] += 1;
-				if ($arr_of_colleges[$college][$new_branch][0] > 0)
 					$arr_of_colleges[$college][$new_branch][0] -= 1;
+					
+					$arr_of_students[$k]["new_branch"] = $new_branch;
+					
+					$arr_of_students = parse($arr_of_students, $k);
+					
+				}
 				
-				$name = mysql_result($result, $j, "Name");
-				
-				$parent_name = mysql_result($result, $j, "Parent_Name");
-				$percentage = mysql_result($result, $j, "Percentage");
-				
-				$query_final = "INSERT INTO upgradation_result (Roll_No, Name, Parent_Name, Institute, Branch_Old, Branch_New, Percentage)
-									VALUES ('$roll_no', '$name', '$parent_name', '$college', '$branch', '$new_branch', '$percentage')
-									ON DUPLICATE KEY UPDATE Branch_New = '$new_branch';";
-				
-				$result_final = mysql_query($query_final, $con) or die(mysql_error().'<br>'.$query_final);
-			
+						
 			}
 		
 		}
 		
-		$arr_roll_nos = array_unique($arr_roll_nos);
-		return 0;
+		return $arr_of_students;
+	}
 	
+	
+	
+	//Cleaner Function
+	function clean($str) {
+		
+		$str = str_replace("'", "", $str);
+		$str = str_replace("/", "", $str);
+		$str = str_replace("\\", "", $str);
+		$str = str_replace(" ", "", $str);
+		
+		return $str;
 	}
 
 ?>
